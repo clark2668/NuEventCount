07a9c71f59f0c0e5f3cc707cb83ab349422a186d
clark2668 <clark.2668@osu.edu>
2018-09-11 16:03:41 -0400

    Can now take command line arguments

--- compute_counts.py
+++ compute_counts.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import sys
 import constants as const
 
-def compute_counts()
+def compute_counts():
 
-		data = np.genfromtxt("file_name",delimiter=',',skip_header=1,names=['energy_logev','veff'])
-		veff=data['veff']
-		energy = np.array([17.,17.5,18.,18.5,19.,19.5,20.,20.5,21.])
+	filename = sys.argv[1]
+	print filename
+	data = np.genfromtxt(filename,delimiter=',',skip_header=1,names=['energy_logev','veff'])
+	veff=data['veff']
+	print veff
+	# energy = np.array([17.,17.5,18.,18.5,19.,19.5,20.,20.5,21.])
 
 compute_counts()
