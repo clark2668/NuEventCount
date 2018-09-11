9b989fe9db5535d80cdb973e30e7c81dfc3ecda2
clark2668 <clark.2668@osu.edu>
2018-09-11 16:31:11 -0400

    Finish Lint calculation
    Finished the Ghandi cross section implementation and first actual return of Lint.

--- tools.py
+++ tools.py
@@ -3,9 +3,14 @@ import numpy as np #import numpy
 import math
 import constants as const
 
-def get_Lint(energy_eV):
+def get_Lint(energy_eV, which_sigma=None):
 	"""
 	get_Lint
+
+	This is a parameterization of the neutrino effective length.
+	By default, it will use a cross section 
+	measurement from Ghandi et. al. (10.1103/PhysRevD.58.093009)
+
 
 	Parameters
 	----------
@@ -17,3 +22,12 @@ def get_Lint(energy_eV):
 	Lint (float): interaction length
 		interaction length of a neutrino in centimeters
 	"""
+	Lint=0.
+
+	if(which_sigma==None):
+		#by default, assuem Ghandi et. al. (10.1103/PhysRevD.58.093009)
+
+		sigma = 7.84e-36 * ((energy_eV/1.e9)**0.363)
+		Lint = const.NucleonMass / (const.EarthDensity * sigma)
+
+	return Lint
