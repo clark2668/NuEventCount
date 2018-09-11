35f948c79a7eb040f1ab70061f0e5d85c4a8dd0a
clark2668 <clark.2668@osu.edu>
2018-09-11 15:56:23 -0400

    Add compute counts shell

---
+++ compute_counts.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import constants as const
+
+def compute_counts()
+
+		data = np.genfromtxt("file_name",delimiter=',',skip_header=1,names=['energy_logev','veff'])
+		veff=data['veff']
+		energy = np.array([17.,17.5,18.,18.5,19.,19.5,20.,20.5,21.])
+
+compute_counts()
