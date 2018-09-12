3890b30e7e39c25467ba6c79c46daf9c02341529
Brian Clark <clark.2668@osu.edu>
2018-09-11 20:39:59 -0400

    Merge fluxes class into tools class
    Put the fluxes class into the tools class. No good reason right no for
    them to be separate.

--- tools.py
+++ tools.py
@@ -31,3 +31,40 @@ def get_Lint(energy_eV, which_sigma=None):
 		Lint = const.NucleonMass / (const.EarthDensity * sigma)
 
 	return Lint
+
+
+def get_flux(resource_name, energy_vals_logeV):
+	"""
+	get_flux
+
+	Return the flux of neutrinos as a function of energy
+
+
+	Parameters
+	----------
+	resource_name: name of the flux you want
+		name of the flux you want
+	energy_vals_logeV:
+		the energies you want the flux evaluted at, in units of log10(eV)
+
+	Returns
+	-------
+	energy_bins: ndarray
+		the energy bins the limit is defined over in the units of log10(eV)
+	flux_prediction: ndarray
+		the flux prediction in units of 1/cm^2/s/sr
+
+	"""
+	flux = np.array([])
+
+	if(resource_name=='icecube_thrumu'):
+		def icecube_thrumu_function(E):
+			return 3.03 * ((E/1.e14)**-2.19) * 1e-27
+		flux = icecube_thrumu_function(np.power(10.,energy_vals_logeV))
+
+	elif(resource_name=='icecube_combined'):
+		def icecube_combined_function(E):
+			return 6.7 * ((E/1.e14)**-2.50) * 1e-27
+		flux = icecube_combined_function(np.power(10.,energy_vals_logeV))
+
+	return flux
